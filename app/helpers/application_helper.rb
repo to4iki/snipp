745cde466aea64250edcfc73d4ec4145764e485f
tsk takezawa <tsk.take815@gmail.com>
2013-02-16 14:00:00 +0900

    デザイン修正
    アイコン追加
    モーダルウィンドウ追加
    アコーディオン表示削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def current_action_classes
     %W[#{controller_name}_controller #{action_name}_action]
   end
+
+  # icon表示用のヘルパーメソッド
+  def link_to_with_icon(icon_type, path, options = {})
+    link_to path, options do
+      content_tag(:i, :class => "icon-#{icon_type}") {}
+    end
+  end
+
 end
